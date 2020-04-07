923b7d701d96f973d75dd369a90d223097616f2e
LINKdiscordd <50463727+LINKdiscordd@users.noreply.github.com>
2020-04-07 21:54:07 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-const filehost = require("./index.js")
-
-filehost.host("./.circleci")
-
-filehost.port(3000)
-
+try {
+  const filehost = require("./index.js")
+  
+  filehost.host("./.circleci")
+  filehost.port(3000)
+    
+} catch(er) {
+  console.error(er)
+}
