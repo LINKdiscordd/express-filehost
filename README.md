5a922d67f3231b4c5f8fb6211049db45d1156a14
LINKdiscordd <50463727+LINKdiscordd@users.noreply.github.com>
2020-04-07 22:36:48 +0200

    Deleted bruh,txt and Updated index.js and README.md

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # express-filehost
 Allows you to host the content from a folder and then access it through a **GET method** on the **/files** endpoint
 **/files/(file)** or **/files/(folder)/(file)** etc.
+the default is /files but can also be changed
 
 # Usage
 ```javascript
-const filehost = require("express-filehost")
+const filehost = require("express-filehost") 
 
-filehost.host("./example") //The folder you want to use (Only 1 folder)
-filehost.port(3000) //The port you want to use
+filehost.host("./example-folder", "examples") 
+//the second argument is "files" by default which results to the endpoint being /files
+
+//   2 folders can NOT use the same endpoint
+
+filehost.port(3000) //use the port 3000
 ```
 
 # Install
--- index.js
+++ index.js
@@ -5,13 +5,18 @@ function isString(x) {
     return typeof (x) == "string";
 }
 
-function host(path) {
+function host(path, endpointt) {
     if (!path || !isString(path)) return new Error("Argument must be a string")
+
+    let endpoint;
+
+    if (!endpointt || !isString(endpointt)) endpoint = "files"
+    else endpoint = endpointt
 
     const fs = require("fs")
     if (!fs.existsSync(path)) return new Error("Path doesn't exist")
 
-    app.get("/files/*", (req, res) => {
+    app.get("/"+endpoint+"/*", (req, res) => {
         let fileRequested = req.url.split("/")
         fileRequested = fileRequested.filter(r => r != '')
         fileRequested.shift()
@@ -20,8 +25,8 @@ function host(path) {
         if (!fs.existsSync("./" + path + "/" + fileRequested)) return res.status(404).send("That file doesn't exist or has been moved")
         res.sendFile(`${__dirname}/${path}/${fileRequested}`)
     })
-    
-    console.log("Filehost: Files and folders inside "+path+" are now accessible")
+
+    console.log(path + "is now accessible")
 }
 
 function port(port) {
